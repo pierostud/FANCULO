0520ded4398caafd98d97f16cd87811030364f47
pierostud <piero.amato66@gmail.com>
2021-08-22 14:53:04 +0200

    Initial Commit

---
+++ FANCULO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FANCULO
+//
+//  Created by PieroAdmin on 22/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
